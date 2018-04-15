5e7fadb40c044908d3e25ae368494de35bcb1f0e
Romain Lesur <romain.lesur@gmail.com>
2018-04-15 14:16:46 +0200

    progress in refactoring

--- R/game.R
+++ R/game.R
@@ -1,6 +1,6 @@
-#' @import R6
+#' @import R6 assertthat
 #' @importFrom yaml read_yaml
-#' @importFrom assertthat assert_that "%has_name%"
+#' @importFrom stringr str_split
 NULL
 
 GameCatalog <- R6Class("GameCatalog",
@@ -22,17 +22,27 @@ GameCatalog <- R6Class("GameCatalog",
 
 
 #' @export
-Game <- R6Class("Game",
+HTML5Game <- R6Class("HTML5Game",
   public = list(
+    # name of the game, it will be the name of the directory inside h
     name = NULL,
     github = NULL,
     use_servr = NULL,
+    # relative path inside the directory
     path = NULL,
     img = NULL,
     author = NULL,
     description = NULL,
-    installed = NULL,
     initialize = function(name, github = NULL, use_servr, path, img = NULL, author = NULL, description = NULL) {
+      assert_that(is.string(name))
+      if (!is.null(github)) assert_that(is.string(github))
+      assert_that(length(stringr::str_split(github, "/")[[1]]) == 2, msg = "Invalid github argument.")
+      assert_that(is.scalar(use_servr))
+      assert_that(is.logical(use_servr))
+      assert_that(is.string(path))
+      if (!is.null(img)) assert_that(is.string(img))
+      if (!is.null(author)) assert_that(is.string(author))
+      if (!is.null(description)) assert_that(is.string(description))
       self$name <- name
       self$github <- github
       self$use_servr <- use_servr
@@ -45,7 +55,8 @@ Game <- R6Class("Game",
       if (missing(use_servr))
         use_servr <- self$use_servr
       tmp_dir <- self$copy_ressources()
-      utils::browseURL(file.path(tmp_dir, self$name, self$path))
+      #utils::browseURL(file.path(tmp_dir, self$name, self$path))
+      self$launch_game(tmp_dir)
     },
     copy_ressources = function() {
       game_dir <- system.file("h", self$name, package = "Rcade", mustWork = TRUE)[1]
@@ -57,10 +68,45 @@ Game <- R6Class("Game",
         NULL
       }
     },
-    is_installed = function() {
-      game_dir <- system.file("h", self$name, package = "Rcade")[1]
-      game_dir != ""
+    launch_game = function(tmp_dir) {
+      if (!is.null(tmp_dir)) {
+        file.path_args <- as.list(c(tmp_dir, self$name, stringr::str_split(self$path, "/")[[1]]))
+        game_file <- do.call(file.path, args = file.path_args)
+        #game_file <- file.path(tmp_dir, self$name, self$path)
+        getOption("viewer")(game_file, "maximize")
+      }
+    },
+    install = function() {
+      repo_name <- stringr::str_split(self$github, "/")[[1]][2]
+      url <- paste0("https://github.com/", self$github, "/archive/master.zip")
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd), add = TRUE)
+      zipfile <- paste0(self$name, ".zip")
+      download(url, zipfile, mode = 'wb')
+      utils::unzip(zipfile)
+      assert_that(file.rename(paste0(repo_name, "-master"), self$name),
+                  msg = "Unable to rename temporary directory.")
+      dest_dir <- system.file("h", package = "Rcade")[1]
+      assert_that(file.copy(self$name, dest_dir, recursive = TRUE),
+                  msg = paste0("Unable to copy source game in ", dest_dir, "."))
+      invisible(self)
     }
-
+  ),
+  active = list(
+    installed = function() nzchar(system.file("h", self$name, package = "Rcade")[1])
   )
 )
+
+#' @export
+as.list.HTML5Game <- function(x, ...) {
+  l <- list(x$github, x$use_servr, x$path, x$img, x$author, x$description)
+  names(l) <- c("github", "use_servr", "path", "img", "author", "description")
+  game <- list(l)
+  names(game) <- x$name
+  game
+}
+
+# a <- HTML5Game$new("2048", path = "index.html", use_servr = FALSE)
+# mario <- HTML5Game$new("mario", "RcadeRepos/mariohtml5", FALSE, "main.html")
+# a
+# sapply(c(a, a), as.list)
--- R/utils.R
+++ R/utils.R
@@ -1,42 +1,81 @@
-#' @importFrom yaml read_yaml
+#' @import utils
 NULL
 
-installed_games <- function() {
-  dir(system.file("h", package = "Rcade"))
+# download utils ----------------------------------------------------------
+# from webshot
+is_windows <- function() .Platform$OS.type == "windows"
+
+download <- function(url, destfile, mode = "w") {
+  if (getRversion() >= "3.3.0") {
+    download_no_libcurl(url, destfile, mode = mode)
+
+  } else if (is_windows() && getRversion() < "3.2") {
+    # Older versions of R on Windows need setInternet2 to download https.
+    download_old_win(url, destfile, mode = mode)
+
+  } else {
+    utils::download.file(url, destfile, mode = mode)
+  }
 }
 
-games_metadata <- function() {
-  yaml::read_yaml(system.file("games.yml", package = "Rcade"))
+# Adapted from downloader::download, but avoids using libcurl.
+download_no_libcurl <- function(url, ...) {
+  # Windows
+  if (is_windows()) {
+    method <- "wininet"
+    utils::download.file(url, method = method, ...)
+
+  } else {
+    # If non-Windows, check for libcurl/curl/wget/lynx, then call download.file with
+    # appropriate method.
+
+    if (nzchar(Sys.which("wget")[1])) {
+      method <- "wget"
+    } else if (nzchar(Sys.which("curl")[1])) {
+      method <- "curl"
+
+      # curl needs to add a -L option to follow redirects.
+      # Save the original options and restore when we exit.
+      orig_extra_options <- getOption("download.file.extra")
+      on.exit(options(download.file.extra = orig_extra_options))
+
+      options(download.file.extra = paste("-L", orig_extra_options))
+
+    } else if (nzchar(Sys.which("lynx")[1])) {
+      method <- "lynx"
+    } else {
+      stop("no download method found")
+    }
+
+    utils::download.file(url, method = method, ...)
+  }
 }
 
-available_games <- function() {
-  names(games_metadata())
+
+# Adapted from downloader::download, for R<3.2 on Windows
+download_old_win <- function(url, ...) {
+  # If we directly use setInternet2, R CMD CHECK gives a Note on Mac/Linux
+  seti2 <- `::`(utils, 'setInternet2')
+
+  # Check whether we are already using internet2 for internal
+  internet2_start <- seti2(NA)
+
+  # If not then temporarily set it
+  if (!internet2_start) {
+    # Store initial settings, and restore on exit
+    on.exit(suppressWarnings(seti2(internet2_start)))
+
+    # Needed for https. Will get warning if setInternet2(FALSE) already run
+    # and internet routines are used. But the warnings don't seem to matter.
+    suppressWarnings(seti2(TRUE))
+  }
+
+  method <- "internal"
+
+  # download.file will complain about file size with something like:
+  #       Warning message:
+  #         In download.file(url, ...) : downloaded length 19457 != reported length 200
+  # because apparently it compares the length with the status code returned (?)
+  # so we supress that
+  utils::download.file(url, method = method, ...)
 }
-
-is_available <- function(name) {
-  name %in% available_games()
-}
-
-game <- function(name) {
-  assertthat::assert_that(assertthat::is.string(name))
-  assertthat::assert_that(is_available(name), msg = "game not available.")
-  structure(c(list(name = name), games_metadata()[[name]]), class = "RcadeGame")
-}
-
-as.list.RcadeGame <- function(x, ...) {
-  l <- list(x[-1])
-  names(l) <- x[[1]]
-  l
-}
-
-install <- function(x, ...) UseMethod("install", x)
-
-download <- function(x, ...) UseMethod("download", x)
-
-as_yaml <- function(x, ...) UseMethod("as_yaml", x)
-
-#' @export
-as_yaml.RcadeGame <- function(x, ...) {
-  yaml::as.yaml(as.list(x))
-}
-
